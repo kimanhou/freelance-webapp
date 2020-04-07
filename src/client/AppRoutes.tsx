30c99d6cb0ef456c7023da3f72ab60648cc4e69a
Kim Anh Nguyen <kimanhou@gmail.com>
2020-04-07 16:59:29 +0200

    Change background image every 3 sec

--- src/client/AppRoutes.tsx
+++ src/client/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Route, Switch, useLocation } from 'react-router-dom';
 import './AppRoutes.scss';
 import RouteModel from './RouteModel';
@@ -11,8 +11,29 @@ const AppRoutes : React.FC = props => {
     const location = useLocation();
     const isIndex = location.pathname == null || location.pathname == "/";
     const routesVisibleClassName = (isIndex) ? "" : "visible";
+    const isBackgroundImageVisibleClassName = (isIndex) ? "visible" : "";
+    const backgroundImages = [`./DSC_0217.jpg`, `./DSC_0104.jpg`, `./DSC_0441.jpg`];
+    const backgroundImageIdexRef = useRef(0);
+    const [backgroundUrl, setBackgroundUrl] = useState(backgroundImages[backgroundImageIdexRef.current]);
+    useEffect(() => {
+        const interval = setInterval(changeBackgroundUrl, 3000);
+        return () => clearInterval(interval);
+    }, []);
+    const changeBackgroundUrl = () => {
+        const idexCurrentUrl = backgroundImageIdexRef.current;
+        if (idexCurrentUrl == backgroundImages.length - 1) {
+            setBackgroundUrl(backgroundImages[0]);
+            backgroundImageIdexRef.current = 0;
+        }
+        else {
+            setBackgroundUrl(backgroundImages[idexCurrentUrl + 1]);
+            backgroundImageIdexRef.current = idexCurrentUrl + 1;
+        }
+    }
+    
     return (
         <div className={`app-routes`}>
+        <img className={`image ${isBackgroundImageVisibleClassName}`} src={backgroundUrl}/>
           <AppHeader centerNavigationBar={isIndex} />
           <div className={`app-routes-content ${routesVisibleClassName}`}>
             <Switch>
