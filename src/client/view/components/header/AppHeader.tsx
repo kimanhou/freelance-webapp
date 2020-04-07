5ee45ba18fbb1bbe6db96b87f3f666d96d4d6640
Kim Anh Nguyen <kimanhou@gmail.com>
2020-04-07 14:12:31 +0200

    Click on website title goes to Home

--- src/client/view/components/header/AppHeader.tsx
+++ src/client/view/components/header/AppHeader.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import NavigationBar from '../navigation/NavigationBar';
+import { useHistory } from 'react-router-dom';
+import './AppHeader.scss';
 
 interface IHeaderProps{
     centerNavigationBar : boolean;
 }
 
 const AppHeader : React.FC<IHeaderProps> = props => {
+    const history = useHistory();
+    const goToHomePage = () => {
+        history.push("/");
+    }
+
     return (
         <div className={`header`}>
-          <h1>{`Kim Anh & Mark`}</h1>
+          <h1 className={`website-title`} onClick={goToHomePage}>{`Kim Anh & Mark`}</h1>
           <NavigationBar isVerticallyCentered={props.centerNavigationBar}/>
         </div>
     )
